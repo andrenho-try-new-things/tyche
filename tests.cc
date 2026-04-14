ce1e04aea7c3d07be5c7c5c5f89622da9a2f35b1
Andre Wagner <andre.nho@gmail.com>
2026-04-14 16:42:09 -0500

    .

--- tests.cc
+++ tests.cc
@@ -8,17 +8,16 @@ TEST(Lexer, Lexer)
     using T = Token;
     auto t = tokenize;
 
-    /*
     ASSERT_EQ(t("42"), (V{ T(Integer(42)), T(EOF_()) }));
     ASSERT_EQ(t("42;"), (V{ T(Integer(42)), T(Symbol(";")), T(EOF_()) }));
     ASSERT_EQ(t("@= 42;"), (V{ T(Symbol("@=")), T(Integer(42)), T(Symbol(";")), T(EOF_()) }));
     ASSERT_EQ(t("@=;"), (V{ T(Symbol("@=")), T(Symbol(";")), T(EOF_()) }));
     ASSERT_EQ(t("@=id;"), (V{ T(Symbol("@=")), T(Identifier("id")), T(Symbol(";")), T(EOF_()) }));
-     */
     ASSERT_EQ(t(R"(hello = "Hello world!")"), (V{ T(Identifier("hello")), T(Symbol("=")), T(String("Hello world!")), T(EOF_()) }));
     ASSERT_EQ(t(R"("a\nb")"), (V{ T(String("a\nb")), T(EOF_()) }));
     ASSERT_EQ(t(R"("a\"b")"), (V{ T(String("a\"b")), T(EOF_()) }));
     ASSERT_EQ(t(R"("a\\b")"), (V{ T(String("a\\b")), T(EOF_()) }));
+    ASSERT_EQ(t(R"("a\gb")"), (V{ T(String("a\\gb")), T(EOF_()) }));
 }
 
 int main(int argc, char** argv)
